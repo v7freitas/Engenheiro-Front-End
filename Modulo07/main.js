5c79fd0b8284464ba030baccc70e633e3f8f2124
Vinícius Freitas <v_freitas@hotmail.com.br>
2023-07-17 22:47:34 -0300

    correção para conseguir interpolar

--- Modulo07/main.js
+++ Modulo07/main.js
@@ -3,23 +3,28 @@ const form = document.getElementById('form-deposito');
 function validaNome(nomeCompleto){
     const nomeComoArray = nomeCompleto.split(' ');
     return nomeComoArray.length >= 2;
-
 }
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e){
     let formEValido = false;
     e.preventDefault();
 
     const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = 'Montante de: ${valorDeposito} depositado para o cliente: ${nomebeneficiario} - conta: ${numeroContaBeneficiario}';
-
+    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
+    
     formEValido = validaNome(nomeBeneficiario.value)
-    if (formEValido) {
+    if (formEValido ){
         alert(mensagemSucesso);
-    } else {
-        alert('O nome não está completo');
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+
+    }    
+    else {
+        alert("O nome não está completo");
     }
 })
 
