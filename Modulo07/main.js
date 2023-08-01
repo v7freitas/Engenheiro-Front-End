e6317abcf6e842df69c7f35064e3fe696b57ec0f
Vinícius Freitas <v_freitas@hotmail.com.br>
2023-08-01 15:49:49 -0300

    código finalizado na última aula

--- Modulo07/main.js
+++ Modulo07/main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEValido = false;
 
 function validaNome(nomeCompleto){
     const nomeComoArray = nomeCompleto.split(' ');
@@ -6,26 +8,36 @@ function validaNome(nomeCompleto){
 }
 
 form.addEventListener('submit', function(e){
-    let formEValido = false;
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de:<strong> ${valorDeposito.value}</strong> depositado para o cliente: <strong>${nomeBeneficiario.value}</strong> - conta: <strong>${numeroContaBeneficiario.value}</strong>`;
+    const mensagemSucesso = `Montante de: <strong> ${valorDeposito.value}</strong> depositado para o cliente: <strong>${nomeBeneficiario.value}</strong> - conta: <strong>${numeroContaBeneficiario.value}</strong>`;
     
     formEValido = validaNome(nomeBeneficiario.value)
-    if (formEValido)  {
-        document.querySelector('success-message').innerHTML = mensagemSucesso;
+    if (formEValido) {
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block;'
 
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         valorDeposito.value = '';
-
-    }    
-    else {
-        alert("O nome não está completo");
+    } else {
+        nomeBeneficiario.style.border = '1px solid red'
+        document.querySelector('.error-message').style.display = 'block';
     }
 })
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup', function(e){
+    console.log(e.target.value);
+    formEValido = validaNome(e.target.value)
+
+    if (!formEValido) {
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error-message').style.display = 'block';
+    } else {
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+})
